1f88b8cedc91ddaad2830f866ac92109a4978a93
david.vega@snhu.edu <david.vega@snhu.edu>
2018-08-31 20:27:09 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(myArray, element){
+    return [element, ...myArray];
+};
+
+function destructivelyAddElementToBeginningOfArray (myArray, element){
+  myArray.unshift(element);
+  return myArray
+}
